909522819335e6920093edcf5a44e3f245c50a8c
Mariano Kurke <kurkemariano@gmail.com>
2024-08-12 22:56:32 -0300

    changes in folders

---
+++ backend/sql/create_tables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS productos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INTEGER NOT NULL
+);
